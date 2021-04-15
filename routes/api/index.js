cb7547360d68b9ae8f03416862dc5645884169a2
Tomara Petty <tomararuth@gmail.com>
2021-04-14 18:00:56 -0700

    adding routes and controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,18 +1,18 @@
-const router = require("express").Router();
-const exercisesRoutes = require("./exercises")
-
+const router = require('express').Router();
 const workout_controller = require('../../controllers/workout_controller');
+path = require('path');
 
-router.route('/')
-        .get(workout_controller.getWorkouts)
-        .post(workout_controller.createWorkout)
+router
+  .route('/')
+  .get(workout_controller.findAll)
+  .post(workout_controller.create);
 
-router.put("/:id", workout_controller.updateWorkout)
+router
+  .route('/:id')
+  .get('/range', workout_controller.findById)
+  .put('/:id', workout_controller.update)
+  .delete('/delete/:id', workout_controller.remove);
 
-router.delete("/delete/:id", workout_controller.deleteWorkout)
-
-router.get("/range", eworkout_controller.getWorkouts)
-
-router.use("/workouts", exercisesRoutes)
+router.use('/workouts', exercisesRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const path = require('path');
 
-const app = express();
-
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+app.use(routes);
+
+// Connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 require("./routes/api/index")(app);
 
