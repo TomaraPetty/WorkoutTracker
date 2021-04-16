dfa37e3f420344781b1f3e229fc8e6af09f5a828
Tomara Petty <tomararuth@gmail.com>
2021-04-16 15:02:15 -0700

    adding html index.js to routes.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const router = require('express').Router();
-const exercisesRoutes = require('./api/exerciseRoutes');
+const exercisesRoutes = require('./api');
+const htmlRoutes = require('./html')
 
 // API Routes
 router.use('/workouts', exercisesRoutes);
-
-// If no API routes are hit
-router.use(function (req, res) {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
-      });
+router.use('/', htmlRoutes)
 
 module.exports = router;
